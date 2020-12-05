b1013851bade55ab31a565d8f490699e5e9fc276
Mario Juarez <mario@mjp.one>
2020-12-05 10:40:56 +0100

    Fix template

--- _template/src/main.rs
+++ _template/src/main.rs
@@ -3,6 +3,6 @@ use helpers::{input_lines, run};
 fn main() {
     let input: Vec<&str> = input_lines(include_str!("../input.txt"));
 
-    run("part1", &input, || dayN::part1(&input));
-    run("part2", &input, || dayN::part1(&input));
+    run("part1", || dayN::part1(&input));
+    run("part2", || dayN::part2(&input));
 }
