3655600e0bfc79a53a59d5c912a8f618be89c9f7
Mario Juarez <mario@mjp.one>
2020-12-17 23:53:43 +0100

    Fix second day solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -4,5 +4,5 @@ fn main() {
     let input: Vec<&str> = input_lines(include_str!("../input.txt"));
 
     run("part1", || day2::part1(&input));
-    run("part2", || day2::part1(&input));
+    run("part2", || day2::part2(&input));
 }
